12b93233f2c33474293039ae5fdec56aae83ba82
CosmicTacoCat <tacocatdx@gmail.com>
2022-09-29 17:37:10 -0500

    Made a hidden canvas for editing and saving the image. Preparing to add more canvases...

--- labelmaker.js
+++ labelmaker.js
@@ -4,14 +4,19 @@
 let mainCanvas = document.getElementById("main-canvas");
 let mainctx = mainCanvas.getContext("2d");
 mainCanvas.style.display = "none";
+mainctx.font = "10px Helvetica";
 
 /** @type {HTMLCanvasElement} */
 let displayCanvas = document.getElementById("display-canvas");
 let displayctx = displayCanvas.getContext("2d");
 
 let imageInput = document.getElementById("image-upload");
-let imageInput2 = document.getElementById("image-upload2");
-let tempbutton = document.getElementById("tempbutton");
+let textInput = document.getElementById("text-input");
+let addressButton = document.getElementById("add-address");
+
+function updateDisplayCanvas() {
+    displayctx.drawImage(mainCanvas, 0, 0, displayCanvas.width, displayCanvas.height);
+}
 
 // Create an Image object and use the imageFile uploaded to create a URL
 // that the Image object can use.
@@ -31,29 +36,38 @@ function imageHelper(imageFile) {
         // Another canvas for displaying images in reduced size. Main canvas is used for editing.
         displayCanvas.height = mainCanvas.height / 2;
         displayCanvas.width  = mainCanvas.width  / 2;
-        displayctx.drawImage(mainCanvas, 0, 0, image.width / 2, image.height / 2);
+        updateDisplayCanvas();
     }
-}
-
-function imageHelper2(imageFile, x,y,w,h) {
-    let imageURL = URL.createObjectURL(imageFile);
-
-    let image = new Image();
-    image.src = imageURL;
-    // Add an event handler property for this image object
-    image.onload = () => {
-        mainCanvas.height = image.height;
-        mainCanvas.width = image.width;
-        mainctx.drawImage(image, x, y, w, h);
-    }
-}
-
-function tempimage(event) {
-    imageHelper2(imageInput.files[0], 0,0,100,100);
 }
 
 function canvasBGChange(event) {
     imageHelper(event.target.files[0]);
+}
+
+function addAddress(event) {
+    let tokenizedInput = textInput.value.split("\n");
+    let fontSize = getFontSize();
+
+    for (let i = 0; i < tokenizedInput.length; i++) {
+        mainctx.fillText(tokenizedInput[i], 100, 100 + (i * fontSize));
+        updateDisplayCanvas();
+    }
+    
+}
+
+let fontSlider = document.getElementById("font-slider");
+function getFontSize() {
+    return fontSlider.value;
+}
+
+let fontSliderLabel = document.getElementById("font-slider-label");
+function setFontLabel() {
+    fontSliderLabel.innerHTML = `${getFontSize()}px`;
+}
+
+function fontSizeUpdate() {
+    setFontLabel();
+    mainctx.font = `${getFontSize()}px Helvetica`;
 }
 
 function saveImage(type) {
@@ -64,20 +78,23 @@ function saveImage(type) {
 // This app only needs one image uploaded at a time to be used as a background.
 // So changing
 imageInput.addEventListener("change", canvasBGChange, false);
-imageInput2.addEventListener("change", canvasBGChange, false);
-tempbutton.addEventListener("click", tempimage, false);
+addressButton.addEventListener("click", addAddress, false);
+fontSlider.addEventListener("input", fontSizeUpdate, false);
 
-// ToDo: It's not necessary but for the sake of learning, I want to figure out how to drawImage twice.
-// Right now it seemingly erases the previous image if I call the imageHelper function twice.
-// I think I found that it's not actually erasing.
-// Two things that can happen that seem to have this effect:
-// First, the canvas is resizing to the latest image's dimensions. Uploading a smaller image second would downsize the canvas.
-// Second, when trying out imageHelper2, I drew the image with resized (smaller) dimensions but
-//  still resized the canvas to be the full size. The resizing itself seems to clear the canvas.
-//  Side note: If I wanted to resize the canvas without loss of drawn pixels, I can use the getImageData function.
-//   Alternatively, I could keep a list of images in use and their properties in a stack. Depends, for a game maybe, for simple drawing that's overly complex.
 
-// ToDo: I want to resize the source image for displaying on the webpage but I don't want the output image
-//  to be resized. I think I will have an canvas that isn't being displayed as the main canvas and a second
-//  canvas to take the main canvas' image and display it smaller.
-//  Editing the image and saving it will be done using the main canvas.
+/* Notes:
+- Because the display canvas is a smaller version of the main canvas, the text drawn will visually be smaller
+    than what is on the main canvas. Currently, if you tried one background image and then a larger one,
+    the text would *look* bigger on screen because the canvas isn't scaled down to fixed dimensions.
+    Having the display canvas be one size and scaling the image of the main canvas to fit the largest
+    dimension would fix this.
+
+- I want to be able to adjust the position of the text visually, probably using sliders but dragging a box
+    would be even cooler. There are some problems though. Displaying anything on the canvas means drawing
+    pixels onto it. That means I can't just draw text and readjust the position without redrawing the
+    background image and anything else on canvas.
+    A solution is to have two canvases to edit on, each acting as a layer. I then draw the farthest back
+    layer, the background image, onto the display canvas. Next, draw the text layer onto the display canvas.
+    Saving the image will now require a new canvas in which the same process done to the display canvas will
+    be done. However, this canvas will be the same size as the main canvas (the background image canvas).
+*/
